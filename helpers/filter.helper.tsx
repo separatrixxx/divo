d0345d156bf509afd556793d63f6251675be5cdd
separatrixxx <nikitalochmatov@gmail.com>
2024-10-11 04:05:28 +0300

    fix filter

--- helpers/filter.helper.tsx
+++ helpers/filter.helper.tsx
@@ -4,11 +4,9 @@ import { ModelItem } from "../interfaces/models.interface";
 export function filterModels(sort: 'all' | 'collection', models: ModelItem[]): ModelItem[] {
     return models.filter(el => {
         if (sort === 'all') {
-            return true;
-        } else if (sort === 'collection') {
+            return !el.user_voted;
+        } else {
             return el.user_voted;
         }
-
-        return false;
     });
 }

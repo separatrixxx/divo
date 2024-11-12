04011eb9ba9e8e89593a0665005e69410cc6440d
separatrixxx <nikitalochmatov@gmail.com>
2024-11-12 22:15:04 +0300

    update start video

--- layout/TelegramProvider.tsx
+++ layout/TelegramProvider.tsx
@@ -30,7 +30,7 @@ export const TelegramProvider = ({ children }: { children: React.ReactNode }) =>
     if (!firstVisit) {
       setTimeout(() => {
         dispatch(toggleFirstVisit());
-      }, 4500);
+      }, 5500);
     }
   }, [firstVisit, router, dispatch]);
 

2be22435293a5729fe660d1290bb9bd918d9e138
separatrixxx <nikitalochmatov@gmail.com>
2024-12-02 20:22:42 +0300

    fix num format && start screen

--- helpers/format.helper.tsx
+++ helpers/format.helper.tsx
@@ -3,9 +3,9 @@ import { format } from "date-fns";
 
 export function numFormat(num: number): string {
     if (num >= 1000 && num < 1000000) {
-        return Math.round(num / 1000).toFixed(1) + 'k';
+        return (num / 1000).toFixed(1) + 'k';
     } else if (num >= 1000000) {
-        return Math.round(num / 1000000).toFixed(1) + 'M';
+        return (num / 1000000).toFixed(1) + 'M';
     }
 
     return String(num);

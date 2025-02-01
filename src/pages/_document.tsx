769d0cd6dd3c518217decdf101404943198b2a3e
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2025-02-01 13:10:57 +0300

    fix zoom

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -60,6 +60,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </Head>
       <body>
         <Main />
